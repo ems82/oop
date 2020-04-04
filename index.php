376aa633e8784f33bc627fd063e23c063b68601f
ems <ems82@ya.ru>
2020-04-04 23:06:23 +0300

    ООП--2

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
 class User {
     public $name = "Имя";
-    public $password = "Пароль";
-    public $email = "Мыло";
-    public $city = "город";
+
     public function Hello() {
         echo "Hello {$this->name}";
         echo "Hello". $this->name;
     }
 }
 $admin = new User();
-$admin->Hello;
+$admin->name = "Максим";
+$admin->Hello();
